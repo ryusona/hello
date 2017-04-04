db561a2d0ddf50aaf35ca12e23007e0882725b54
danawacomputer <sona.ria.sss@gamil.com>
2017-04-04 11:06:29 +0900

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,10 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class HelloWorld {
+    public static void main(String args[]){
+
+        System.out.print("Hello World");
+
+    }
+}
---
+++ src/MyAcoount.java
@@ -0,0 +1,14 @@
+/**
+ * Created by danawacomputer on 2017-04-04.
+ */
+public class MyAcoount {
+    public static void main(String args[]){
+
+        int myBusCharge = 100;
+        int myTrainCharge = 1000;
+
+        int ChargeHap = myBusCharge + myTrainCharge ;
+        System.out.print("사용한 총 금액은 : " + ChargeHap + " 입니다.");
+
+    }
+}
