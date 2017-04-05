be88efbfdd7036384be8bf7b2ad2cc04f7306e7a
danawacomputer <sona.ria.sss@gamil.com>
2017-04-05 10:43:45 +0900

    update version.pw.1

--- src/ScannerTest.java
+++ src/ScannerTest.java
@@ -8,29 +8,58 @@ public class ScannerTest {
     public static void main(String args[]) {
 
         Scanner in = new Scanner(System.in);
+        int allClient = 3;
 
-        System.out.print("단어를 입력하세요 : ");
-        String inputted = in.nextLine(); // 사용자가 엔터 전까지 출력
+        for(int i = 0; i < allClient; i++) {
 
-        String inputtedFirstChar = inputted.substring(0,1); // 첫 번 째 단어
+            System.out.print("비밀번호를 입력하세요 : ");
+            String targetStr = in.nextLine(); // 사용자가 엔터 전까지 출력
+
+            String prefix = "$$";
+            String postfix = "##";
+
+            int squareLengthOfChar =
+                    targetStr.length() * targetStr.length();
+
+            targetStr =
+                    targetStr.substring(0,1).toUpperCase() + targetStr.substring(1);
+
+            String passwordGenerated = prefix + targetStr + squareLengthOfChar + postfix;
+
+            System.out.println("암호화 된 비밀번호 " + passwordGenerated);
+        }
+
+        System.out.print("총 " + allClient + "명의 비밀번호를 입력했습니다." );
+
+
+        /**
+        String inputtedFirstChar = targetStr.substring(0,1); // 첫 번 째 단어
         String inputtedUpper = inputtedFirstChar.toUpperCase(); // 대문자로 변경
+        // str.substring(0,1).toUpperCase() 첫 단어를 바로 대문자로
 
-        String inputtedslice = inputted.substring(1);
+        String inputtedslice = targetStr.substring(1);
         String inpuutedChange = inputtedUpper + inputtedslice;
 
-        int inputtedLength = inputted.length(); // 글자 수 뽑아내기
+        int inputtedLength = targetStr.length(); // 글자 수 뽑아내기
 
         String result = "$$" + inpuutedChange + (inputtedLength * inputtedLength) + "#" ;
 
-        System.out.println("암호화 된 비밀번호 " + result);
 
-        System.out.print("당신의 비밀번호를 입력하세요 : ");
-        String inputtedUserpw = in.nextLine();
-        String testStr = "seoul";
-        String inputStr = inputtedUserpw;
 
-        if((testStr.equals(inputStr)) == true ) System.out.println("일치 합니다.");
-        else System.out.println("일치 하지않습니다.");
+         * System.out.println("암호화 된 비밀번호 " + result);
+
+         System.out.print("당신의 비밀번호를 입력하세요 : ");
+         String inputtedUserpw = in.nextLine();
+         String testStr = "seoul";
+         String inputStr = inputtedUserpw;
+
+         if((testStr.equals(inputStr)) == true ) System.out.println("일치 합니다.");
+         else System.out.println("일치 하지않습니다.");
+         *
+         *
+         *
+         */
+
 
 
 
